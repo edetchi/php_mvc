4e91e1d8abb441fffe456c1584b5307fd809cf5e
edetchi <contacts@edetchi.com>
2018-11-15 18:24:50 +0900

    12 ルーターとurlが一致した時にプロパティparamsに値をセット、paramsのゲッター作成

--- public/index.php
+++ public/index.php
@@ -6,8 +6,6 @@
  * PHP version 5.4
  */
 
-// echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
-
 /**
  * Routing
  */
@@ -15,14 +13,24 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-//echo get_class($router);
-
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
     
 // Display the routing table
-echo '<pre>';
-var_dump($router->getRoutes());
-echo '</pre>';
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '</pre>';
+
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
